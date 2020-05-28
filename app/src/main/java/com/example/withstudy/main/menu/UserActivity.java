7441565fae69f8bd0c8e9e18598a7b5938555e09
Kimyeongsan <apua34@gmail.com>
2020-05-28 22:09:49 +0900

    Menu 기본 틀 add

--- app/src/main/java/com/example/withstudy/main/menu/UserActivity.java
+++ app/src/main/java/com/example/withstudy/main/menu/UserActivity.java
@@ -4,33 +4,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.withstudy.R;
 import com.example.withstudy.SignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class UserActivity extends AppCompatActivity {
-    Button btnLogOut;
-    FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener authStateListener;
+public class UserActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnLogOut, btnSignOut;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_menu);
-        btnLogOut = (Button) findViewById(R.id.btnLogOut);
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                firebaseAuth.getInstance().signOut();
-                Intent I = new Intent(UserActivity.this, SignInActivity.class);
-                startActivity(I);
+        btnLogOut = findViewById(R.id.btnLogOut);
+        btnSignOut = findViewById(R.id.btnSignOut);
 
-            }
-        });
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        btnLogOut.setOnClickListener(this);
+        btnSignOut.setOnClickListener(this);
     }
+
+    private void logOut() {
+        firebaseAuth.getInstance().signOut();
+    }
+
+    private void signOut() {
+        firebaseAuth.getCurrentUser().delete();
+    }
+
+    @Override
+    // 추가 수정 예정
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnLogOut:
+                logOut();
+                Intent intent = new Intent(
+                        getApplication(),SignInActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btnSignOut:
+                signOut();
+                finishAffinity();
+                break;
+        }
+    }
+
 }
