da3b22d5438e7d0859c941b1354867a7baa07bc7
Kimyeongsan <apua34@gmail.com>
2020-06-03 03:29:38 +0900

    Menu(Log out 버튼 동작)

--- app/src/main/java/com/example/withstudy/main/menu/MenuFragment.java
+++ app/src/main/java/com/example/withstudy/main/menu/MenuFragment.java
@@ -1,22 +1,24 @@
 package com.example.withstudy.main.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 
 import com.example.withstudy.R;
+import com.example.withstudy.SignInUpActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MenuFragment extends Fragment {
-
+    private FirebaseAuth firebaseAuth;
+    private View root;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,8 +29,48 @@ public class MenuFragment extends Fragment {
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_menu, container, false);
+        root = inflater.inflate(R.layout.fragment_menu, container, false);
+        initialize();
 
         return root;
     }
+
+    private void initialize() {
+        Button btnLogOut, btnSignOut;
+
+        btnLogOut = root.findViewById(R.id.btnLogOut);
+        btnSignOut = root.findViewById(R.id.btnSignOut);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+                Intent intent = new Intent(
+                        getContext(), SignInUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getContext(), SignInUpActivity.class);
+                startActivity(intent);
+
+                signOut();
+            }
+        });
+    }
+    private void logOut () {
+        firebaseAuth.getInstance().signOut();
+    }
+
+    private void signOut () {
+        firebaseAuth.getCurrentUser().delete();
+    }
+
 }
